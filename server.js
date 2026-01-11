4707bc6f0acaa523097f73be25096567d6729c04
KaidoKasuk <kaido.kasuk@voco.ee>
2026-01-11 17:42:17 +0200

    added id to url

--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const path = require("path");
-
+const port = process.env.PORT || 8000;
 const app = express();
 //------------first version------------//
 // app.get("/", (req, res) => {
 //   res.send("hello world");
 // });
 
-//setup static folder
-app.use(express.static(path.join(__dirname, "public")));
+//------------second version------------//
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "about.html"));
+// });
+//------------static version------------//
+// //setup static folder
+// app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
+//get all posts
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "about.html"));
+//get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  res.json(posts.filter((post) => post.id === id));
 });
 
-app.listen(8000, () => console.log("server is running port 8000"));
+app.listen(port, () => console.log(`server is running port ${port}`));
