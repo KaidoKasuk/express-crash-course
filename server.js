347e275d65aa23bf61844c93700a92f4194ea8e8
KaidoKasuk <kaido.kasuk@voco.ee>
2026-01-12 11:56:55 +0200

    logger

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
 const port = process.env.PORT || 8000;
 const app = express();
 
@@ -24,6 +25,10 @@ const app = express();
 //------------body parser middleware------------//
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//------------logger middleware------------//
+app.use(logger);
+
 //------------Routes------------//
 app.use("/api/posts", posts);
 
